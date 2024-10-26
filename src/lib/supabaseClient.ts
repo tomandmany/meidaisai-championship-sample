73d5780094f3dd72201e8e01fa390c3dda543253
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-10-27 01:18:43 +0900

    環境変数にPUBLIC_付け足した

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,8 +2,8 @@
 import { Database } from '@/types/supabase.types';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase URL or anon key');
