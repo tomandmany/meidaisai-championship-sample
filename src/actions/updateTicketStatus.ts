68545998cbfefa0d65601cc863d10b2f216351ea
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-10-27 01:04:37 +0900

    updateTicketStatus.tsはコメント化しちゃだめだったから直した

--- src/actions/updateTicketStatus.ts
+++ src/actions/updateTicketStatus.ts
@@ -1,17 +1,17 @@
-// // @/actions/updateTicketStatus.ts
-// 'use server';
-// import { supabase } from '@/lib/supabaseClient';
+// @/actions/updateTicketStatus.ts
+'use server';
+import { supabase } from '@/lib/supabaseClient';
 
-// export async function updateTicketStatus(userId: string) {
-//   const { error } = await supabase
-//     .from('users')
-//     .update({ ticket_used: true }) // `ticket_used` を必ず `true` に設定
-//     .eq('user_id', userId);
+export async function updateTicketStatus(userId: string) {
+  const { error } = await supabase
+    .from('users')
+    .update({ ticket_used: true }) // `ticket_used` を必ず `true` に設定
+    .eq('user_id', userId);
 
-//   if (error) {
-//     console.error('Error updating ticket status:', error.message);
-//     throw new Error('チケットの状態更新に失敗しました');
-//   }
+  if (error) {
+    console.error('Error updating ticket status:', error.message);
+    throw new Error('チケットの状態更新に失敗しました');
+  }
 
-//   return 'チケットの状態が使用済みに更新されました';
-// }
+  return 'チケットの状態が使用済みに更新されました';
+}
