2c6a38c5badd083ee71fec600dccdec52182646a
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2024-10-27 22:20:14 +0900

    /か/votesにアクセスした際のリダイレクト先を環境変数で書き直した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  redirect('/votes/champ');
+  redirect(`${process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL}`);
 };
